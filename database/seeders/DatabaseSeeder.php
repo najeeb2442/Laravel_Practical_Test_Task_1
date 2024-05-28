1ff072dd2b4a8dc0777f9f17870d438b6954e3bf
nagob <s.najib2442@gmail.com>
2024-05-28 12:23:05 +0300

    done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,28 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Product::create([
+            'name' => 'iPhone 15',
+            'description' => '6.1 Super Retina XDR display.
+            Dynamic Island, a magical way to interact with iPhone.
+            A16 Bionic chip with 5-core GPU.
+            Up to 20 hours video playback.
+            USB-C, supports USB 2.
+            FaceTime service depends on network restrictions and is not available when on UAE networks.
+            ',
+            'price' => 294.990,
+        ]);
+        Product::create([
+            'name' => 'samsung s23 ultra',
+            'description' => 'Samsung Galaxy S23 Ultra 5G 256GB 12GB Green Dual Sim Smartphone  Middle East Version',
+            'price' => 314.900,
+        ]);
+        Product::create([
+            'name' => 'huawei p30 pro',
+            'description' => 'Huawei P30 Pro 256GB Aurora 4G Dual Sim Smartphone VOG-L29',
+            'price' => 257.609,
         ]);
     }
 }
